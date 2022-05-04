96368c75d7390cd7d912629826d1a79434a1f3f5
Jiten <jitenshreshtha07@gmail.com>
2022-05-04 11:14:02 +0545

    props done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import C1Greet from './Components/C1Greet';
-import C2Classes from './Components/C2Classes';
+// import C1Greet from './Components/C1Greet';
+// import C2Classes from './Components/C2Classes';
+import C3prop from './Components/C3prop';
 
 function App() {
   return (
     <div>
-      <C1Greet />
-      <C2Classes />
+      {/* <C1Greet /> */}
+      {/* <C2Classes /> */}
+      <C3prop name="Jiten" heroName="Batman"/>
+      <C3prop name="Suchin" heroName="Superman" />
+      <C3prop name="Sundar" heroName="Spiderman" />
     </div>
   );
 }
---
+++ src/Components/C3prop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function C3prop(prop) {
+  return (
+    <div>Hello this is {prop.name} aka {prop.heroName}</div>
+  )
+}
+
+export default C3prop
