cf3c2d42f53430be4a6ad4e7d5e578e953edd92f
Jiten <jitenshreshtha07@gmail.com>
2022-05-04 10:14:35 +0545

    class defined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import C1Greet from './Components/C1Greet';
+import C2Classes from './Components/C2Classes';
 
 function App() {
   return (
     <div>
       <C1Greet />
+      <C2Classes />
     </div>
   );
 }
---
+++ src/Components/C2Classes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class C2Classes extends Component {
+  render() {
+    return (
+      <div>This is class component.</div>
+    )
+  }
+}
+
+export default C2Classes
